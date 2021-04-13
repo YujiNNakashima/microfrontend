397f59130a0d15e4fb48c23080d018dd957b7cd1
Yuji <yujiricardo@gmail.com>
2021-04-13 09:48:28 -0300

    initial commit

---
+++ packages/container/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import MarketingApp from './components/MarketingApp'
+
+export default () => {
+  const el = document.querySelector('#marketing_root')
+
+  return(
+      <>
+      <h1>Hi!</h1>
+      <hr/>
+      <MarketingApp></MarketingApp>
+      </>
+    )
+}
---
+++ packages/container/src/components/MarketingApp.js
@@ -0,0 +1,13 @@
+import { mount as MarketingApp} from 'marketing/MarketingApp'
+import React, { useRef, useEffect } from 'react'
+import { mount } from '../../../marketing/src/bootstrap'
+
+export default () => {
+  const ref = useRef(null)
+
+  useEffect(() => {
+    mount(ref.current)
+  })
+
+  return <div ref={ref}></div>
+}
