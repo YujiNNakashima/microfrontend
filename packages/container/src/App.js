e3c546663f7e79f374551431f3e33d66bfb38d04
Yuji <yujiricardo@gmail.com>
2021-04-17 09:28:55 -0300

    feat: add header

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,14 +1,17 @@
 import React from 'react'
+import { BrowserRouter } from 'react-router-dom'
 import MarketingApp from './components/MarketingApp'
+import Header from './components/Header'
 
 export default () => {
   const el = document.querySelector('#marketing_root')
 
   return(
-      <>
-      <h1>Hi!</h1>
-      <hr/>
-      <MarketingApp></MarketingApp>
-      </>
+      <BrowserRouter>
+        <div>
+          <Header/>
+          <MarketingApp />
+        </div>
+      </BrowserRouter>
     )
   } 
